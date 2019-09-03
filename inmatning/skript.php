4e9c2554fd14e400bf1c8678bf1328095a7f5427
Karim Ryde <karim.ryde@learnet.se>
2019-09-03 14:41:13 +0200

    Formulär, required, name, $_request..

---
+++ inmatning/skript.php
@@ -0,0 +1,24 @@
+<?php
+/* Tar emot data som skickats */
+$förnamn = $_REQUEST["förnamn"];
+$efternamn =  $_REQUEST["efternamn"];
+$mobil = $_REQUEST["mobil"];
+$kön = $_REQUEST["kon"];
+
+var_dump($_REQUEST);
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    /* Skriv ut en snygg bekräftelse */
+    
+    ?>
+</body>
+</html>
