5dcbd23a3fcddc1e9258ab78ced4b633e27ae1cd
karim <karye.webb@gmail.com>
2019-10-30 16:44:08 +0100

    Testar en labb

--- test/scandir.php
+++ test/scandir.php
@@ -8,23 +8,38 @@
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div class="kontainer">
         <h1>Skanna katalog</h1>
         <?php
-$katalog = '.';
+$katalog = './shop-bilder/cpu';
+
+$filer = scandir($katalog);
 
 echo "<table class=\"striped\">";
 echo "<tr><th></th><th>Namn</th></tr>";
-$filer = scandir($katalog);
+
 foreach ($filer as $fil) {
+
+    /* Visa fa-ikoner för dokument och undermappar */
     if (is_dir($fil)) {
-        echo "<tr><td><i class=\"fa fa-folder\"></i></td><td>$fil</td></tr>";
+
+        /* Visa inte ”." och ”.." */
+        if ($fil != '.' && $fil != '..') {
+            echo "<tr><td><i class=\"fa fa-folder\"></i></td><td>$fil</td></tr>";
+        }
     } else {
-        echo "<tr><td><i class=\"fa fa-file\"></i></td><td>$fil</td></tr>";
+
+        /* Visa bilden om filformat ”.jpg”, ”.png”, ”.gif" */
+        $info = pathinfo("./$fil");
+        if ($info['extension'] == 'jpg') {
+            echo "<tr><td><i class=\"fa fa-file\"></i></td><td><img src=\"$katalog/$fil\"></td></tr>";
+        } else {
+            echo "<tr><td><i class=\"fa fa-file\"></i></td><td>$fil</td></tr>";
+        }
     }
 }
 echo "</table>";
--- test/shop.php
+++ test/shop.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bygg din PC</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
-    <link rel="stylesheet" href="shop.css">
+    <link rel="stylesheet" href="./style/shop.css">
 </head>
 <body>
     <div class="kontainer">
