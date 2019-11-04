d143b08c0a3cb936909cd38aff3cfd74667ad76e
karye <karye.webb@gmail.com>
2019-11-04 13:57:35 +0100

    Fixar

--- labb-1/list.php
+++ labb-1/list.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Läsa in csv-fil</title>
-    <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="kontainer">
+        <h1>Alla restauranger</h1>
         <?php
         $filename = './restauranger.csv';
 
