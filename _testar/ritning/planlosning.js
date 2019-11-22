488ab079349b4c3ec0bceb7697f27b84b2184ea0
karye <karye.webb@gmail.com>
2019-11-22 09:05:44 +0100

    Testar svg

--- _testar/ritning/planlosning.js
+++ _testar/ritning/planlosning.js
@@ -1,27 +1,11 @@
-const rect1 = document.querySelector('#rect1');
-const rect2 = document.querySelector('#rect2');
+const svgE = document.querySelector('svg');
 
-/* const useEventType = (typeof window.PointerEvent === 'function') ? 'pointer' : 'mouse';
-const listeners = ['click', 'touchstart', 'touchend', 'touchmove', `${useEventType}enter`, `${useEventType}leave`, `${useEventType}move`];
+svgE.addEventListener("click", boka);
 
-const pointerHandler = (event) => {
-    event.preventDefault();
-    const evtype = document.createTextNode(event.type + "\n");
-    output.appendChild(evtype);
+function boka(e) {
+
+    if (e.target.nodeName = 'RECT') {
+        e.target.classList.toggle("on");
+        console.log("sal click");
+    }
 }
-
-listeners.map((etype) => {
-    circ.addEventListener(etype, pointerHandler);
-});
- */
-
- rect1.addEventListener("click", test1);
- function test1() {
-     rect1.classList.toggle("on");
-     console.log("rect1 click");
- }
- rect2.addEventListener("click", test2);
- function test2() {
-     rect2.classList.toggle("on");
-     console.log("rect2 click");
- }
