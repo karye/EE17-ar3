a20888f2bd48d09192fc7226854eaf77c257968b
karye <karye.webb@gmail.com>
2019-11-26 14:53:19 +0100

    Ny labb

---
+++ labb-5/skriva.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>bloggen</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <h1>Bloggen</h1>
+        <nav>
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a class="nav-link" href="./lasa.php">Läsa</a></li>
+                <li class="nav-item"><a class="nav-link active" href="./skriva.php">Skriva</a></li>
+            </ul>
+        </nav>
+        
+        <?php
+        
+        ?>
+    </div>
+</body>
+</html>
