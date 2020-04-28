8cb33fc697d5fd1cf7df02abbb6c269fda8cc960
karye <karye.webb@gmail.com>
2020-04-28 14:47:14 +0200

    Övat på geolocation, mapbox och ajax

--- api-json/trafiklab/hallplatser.js
+++ api-json/trafiklab/hallplatser.js
@@ -2,31 +2,53 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2FyeWUiLCJhIjoiY2pwOXRtbWc1MGdmNjNwc2JmdGxzeDR5byJ9.whp8f2Ttws57ctAf_stuag';
 
 /* Start positions: NTI */
-var lat = 59.336885;
-var lon = 18.048323;
+var latNTI = 59.336885;
+var lonNTI = 18.048323;
 
 /* Skapa kartan */
-var karta = new mapboxgl.Map({
+var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    center: [lon, lat],
+    center: [lonNTI, latNTI],
     zoom: 10
 });
 
-/* Element vi använder */
-var eDemo = document.querySelector("#demo");
+/* Hämta användarens position */
+getLocation();
 
 /* Om vi får avläsa positionen, kör igång */
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        eDemo.innerHTML = "Geolocation is not supported by this browser.";
+        console.log("Geolocation is not supported by this browser.");
     }
 }
 
 /* Visa användares latitude och longitude */
 function showPosition(position) {
-    eDemo.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
+    var latHem = position.coords.latitude;
+    var lonHem = position.coords.longitude;
+    console.log("Latitude: " + latHem + ", Longitude: " + lonHem);
+
+    /* Infoga en marker på kartan */
+    var marker = new mapboxgl.Marker()
+    .setLngLat([lonHem, latHem])
+    .addTo(map);
+
+    /* Packa in lat och lon till ett POST-paket */
+    var postData = new FormData();
+    postData.append("lat", latHem)
+    postData.append("lon", lonHem);
+
+    /* Skicka lat och lon till php-skriptet mha ajax*/
+    var ajax = new XMLHttpRequest();
+    ajax.open("POST", "trafiklab.php", true);
+    ajax.send(postData);
+
+    /* Väntar på svaret från ajax */
+    ajax.addEventListener("loadend", function() {
+        console.log(this.responseText);
+        
+    })
 }
